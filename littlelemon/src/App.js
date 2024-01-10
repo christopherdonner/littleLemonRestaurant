335810153af46873a34162144f49d028f89e29ac
Christopher Donner <cdonner@opentext.com>
2024-01-10 10:12:11 -0500

    semantic HTML configured with meta OG tags

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {Header} from './header.js'
+import {Nav} from './nav.js'
+import {Main} from  './main.js'
+import {Footer} from './footer.js'
 
 function App() {
   return (
-    <div className="App">
-      Poop
+    <div>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
     </div>
   );
 }
---
+++ littlelemon/src/header.js
@@ -0,0 +1,8 @@
+import littleLemonLogo from "./images/logo.jpg"
+
+export const Header = ()=>{
+
+    return (<header>
+        <img src={littleLemonLogo} alt="trash" height="100px" />
+    </header>)
+}
