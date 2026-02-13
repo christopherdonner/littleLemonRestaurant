f161c02665661b128bf0ed8152b760c82b6437cb
Christopher Donner <cdonner@opentext.com>
2026-02-13 13:27:47 -0500

    fixed booking form

--- littlelemon/src/BookingForm.js
+++ littlelemon/src/BookingForm.js
@@ -2,7 +2,6 @@ import { useState, useReducer, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const BookingForm = (state) => {
-let uzeEffect = useEffect();
    const seededGenerator = (date, hour) => {
       const m = 9;
       const d = date.getDate();
@@ -29,26 +28,19 @@ let uzeEffect = useEffect();
 
    const availableTimesUpdate = (e) => {
       dispatch(fetchAPI(new Date(e.target.value)))
-      uzeEffect((e)=>{
-      setDate(e.target.value);
-
-      })
-      // effect(()=>{
-      // }, [])
-      
    }
 
    const [date, setDate] = useState(""),
-      [time, setTime] = useState(''),
+      [time, setTime] = useState(""),
       [guests, setGuests] = useState(0);
    return <div>
       <form action={state.submitForm({ time: time, date: date, guests: guests })}>
          <label htmlFor="guests">Number of guests</label>
          <input type="number" name="guests" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
          <label htmlFor="res-date">Choose date</label>
-         <input type="date" required id="res-date" value={date} onChange={availableTimesUpdate} min={`${new Date().toLocaleString("default", { year: 'numeric' })}-${new Date().toLocaleString("default", { month: '2-digit' })}-${new Date().toLocaleDateString("default", { day: '2-digit' })}`}></input>
+         <input type="date" required id="res-date" value={date} onChange={e => {setDate(e.target.value); availableTimesUpdate(e)}} min={`${new Date().toLocaleString("default", { year: 'numeric' })}-${new Date().toLocaleString("default", { month: '2-digit' })}-${new Date().toLocaleDateString("default", { day: '2-digit' })}`}></input>
          <label htmlFor="res-time">Choose time</label>
-         <select id="res-time" value={time} required onChange={e => { e.preventDefault();setTime(e.target.value); }} >
+         <select id="res-time" value={time} required onChange={e => { e.preventDefault(); setTime(e.target.value); }} >
             {times.map((time, index) => {
                return <option key={index} value={time}>{time}</option>
             })}
